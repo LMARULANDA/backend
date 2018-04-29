fb90277c8696175b908fdec86e87bdb88b77c7db
laura marulanda <lauris037.lm@gmail.com>
2018-04-29 16:14:24 -0500

    Entity user created

---
+++ src/main/java/com/example/gradlesprint/model/ParentEntity.java
@@ -0,0 +1,38 @@
+package com.example.gradlesprint.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+
+@MappedSuperclass
+//acceda al atributo y no a la propiedad
+@Access(AccessType.FIELD)
+public class ParentEntity implements Serializable {
+
+	private static final long serialVersionUID = -9148994175881530233L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	//name nombre de la base de datos exactamente igual
+	@Column(name="id", unique=true, nullable=false)
+	private long id;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	
+
+	
+}
