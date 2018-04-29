b2823c9e04dd0fadf655aadc62b8a5a931c75149
laura marulanda <lauris037.lm@gmail.com>
2018-04-29 15:21:12 -0500

    first commit

---
+++ src/main/java/com/example/gradlesprint/GradleSprintApplication.java
@@ -0,0 +1,12 @@
+package com.example.gradlesprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleSprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleSprintApplication.class, args);
+	}
+}
